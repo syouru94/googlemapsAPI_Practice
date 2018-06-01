6be9f8e18f75deb0d91d104c2b26e83c8ab65265
syouru94 <xero120017@gmail.com>
2018-06-01 17:35:45 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# GoogleMaps API練習--以台北市拉麵店為例
+
+本範例參考Yeh James所著之文章：
+- https://medium.com/@yehjames/%E8%B3%87%E6%96%99%E5%88%86%E6%9E%90-%E6%A9%9F%E5%99%A8%E5%AD%B8%E7%BF%92-%E7%AC%AC2-2%E8%AC%9B-%E5%A6%82%E4%BD%95%E7%8D%B2%E5%8F%96%E8%B3%87%E6%96%99-google-map-api-beb7c88dc4e3
+
+以練習Google Maps提供之API為目標，裡頭使用了Geocoding API及Places API
+
+以coding內容為例，選取rating>4隻拉麵店，並限定為內湖區之拉麵店，顯示店名、地址、電話評價等資訊
+
+<img src="https://i.imgur.com/HhO6L41.png">
---
+++ ramen.py
@@ -0,0 +1,55 @@
+#%%
+#import相關library，並放入金鑰
+import googlemaps 
+import pandas as pd
+import numpy as np
+
+gmaps = googlemaps.Client(key="你的金鑰")
+geocode_result = gmaps.geocode("中正區")
+geocode_result
+#建立一個包含台北市行政區的list
+districts=["中正區","大同區","中山區","松山區","大安區","萬華區","信義區","士林區","北投區","內湖區","南港區","文山區"]
+
+#以for loop，使用gmaps.geocode取得每個行政區的GPS位位址，搜尋以其為中心半徑2500m中符合的拉麵店家id，
+#並append到變數ids中
+
+ids=[]
+for district in districts:
+    geocode_result=gmaps.geocode(district)
+    #以loc儲存該行政區座標
+    loc = geocode_result[0]['geometry']['location']
+    
+    print("以"+district+"為中心半徑2500公尺的拉麵店家數量: "+str(len(gmaps.places_radar(keyword="拉麵",
+                                                                      location=loc, radius=2500)['results'])))
+    #將符合之拉麵店append進ids list之中
+    for place in gmaps.places_radar(keyword="拉麵", location=loc, radius=2500)['results']:
+        ids.append(place['place_id'])
+#%%
+ids
+#%%
+stores_info = []
+# 去除重複的店家
+ids = list(set(ids))
+for id in ids:
+    stores_info.append
+    stores_info.append(gmaps.place(place_id=id, language='zh-TW')['result'])
+np.array(stores_info)
+
+#%%
+output= pd.DataFrame.from_dict(stores_info)
+output
+#水平合併所需要的資訊
+df_1 = output.loc[:, "formatted_address":"formatted_phone_number"]
+df_2 = output.loc[:, "international_phone_number":"name"]
+df_3= output.loc[:,"rating"]
+df=pd.concat([df_1, df_2,df_3], axis=1)
+#取得店名
+#%%
+
+#選取rating>4之拉麵店
+rating_df=df[df.loc[:,"rating"]>4]
+#整理需要之資訊（地址、電話、國際電話、店名、評價），存入fixed_list中
+fixed_df=rating_df[["formatted_address","name","international_phone_number","rating"]]
+
+#將fixed_df中formatted_address，以.str.contains() function方式，以行政區的郵遞區號，選擇該行政區的拉麵店，在此以114內湖區為例
+rating_df[fixed_df.formatted_address.str.contains("114")]
