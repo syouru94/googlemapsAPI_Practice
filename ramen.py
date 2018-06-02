689619841b89c1387598926736b7ba7079b68214
syouru94 <xero120017@gmail.com>
2018-06-03 00:00:30 +0800

    third commit

--- ramen.py
+++ ramen.py
@@ -5,8 +5,7 @@ import pandas as pd
 import numpy as np
 
 gmaps = googlemaps.Client(key="你的金鑰")
-geocode_result = gmaps.geocode("中正區")
-geocode_result
+
 #建立一個包含台北市行政區的list
 districts=["中正區","大同區","中山區","松山區","大安區","萬華區","信義區","士林區","北投區","內湖區","南港區","文山區"]
 
@@ -19,9 +18,7 @@ for district in districts:
     #以loc儲存該行政區座標
     loc = geocode_result[0]['geometry']['location']
     
-    print("以"+district+"為中心半徑2500公尺的拉麵店家數量: "+str(len(gmaps.places_radar(keyword="拉麵",
-                                                                      location=loc, radius=2500)['results'])))
-    #將符合之拉麵店append進ids list之中
+       #將符合之拉麵店append進ids list之中
     for place in gmaps.places_radar(keyword="拉麵", location=loc, radius=2500)['results']:
         ids.append(place['place_id'])
 #%%
@@ -39,17 +36,20 @@ np.array(stores_info)
 output= pd.DataFrame.from_dict(stores_info)
 output
 #水平合併所需要的資訊
-df_1 = output.loc[:, "formatted_address":"formatted_phone_number"]
-df_2 = output.loc[:, "international_phone_number":"name"]
-df_3= output.loc[:,"rating"]
-df=pd.concat([df_1, df_2,df_3], axis=1)
+df_1 = output.loc[:, "formatted_address"]
+df_2 = output.loc[:, "formatted_phone_number"]
+df_3 = output.loc[:, "name"]
+df_4 = output.loc[:, "rating"]
+
+df = pd.concat([df_1, df_2, df_3, df_4], axis=1)
 #取得店名
 #%%
 
 #選取rating>4之拉麵店
 rating_df=df[df.loc[:,"rating"]>4]
 #整理需要之資訊（地址、電話、國際電話、店名、評價），存入fixed_list中
-fixed_df=rating_df[["formatted_address","name","international_phone_number","rating"]]
+fixed_df = rating_df[["formatted_address",
+                      "name", "formatted_phone_number", "rating"]]
 
 #將fixed_df中formatted_address，以.str.contains() function方式，以行政區的郵遞區號，選擇該行政區的拉麵店，在此以114內湖區為例
 rating_df[fixed_df.formatted_address.str.contains("114")]
